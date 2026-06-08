489bd1d1a93c551e3ce9dd892d58bbb0805a4cfe
itsjackyboi <Jackcasimir@gmail.com>
2026-06-08 19:18:20 -0400

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,1 +1,17 @@
+import os
+import requests
 
+from pintland_calendar import format_message
+
+WEBHOOK_URL = os.environ["DISCORD_WEBHOOK_URL"]
+
+response = requests.post(
+    WEBHOOK_URL,
+    json={
+        "content": format_message()
+    }
+)
+
+response.raise_for_status()
+
+print("Message sent successfully.")
